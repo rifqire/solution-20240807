93d90779253020d06f7261e881a7bcffa4f3a33c
Rifqi RE <rifqi30rachmanda@gmail.com>
2024-08-07 14:07:43 +0700

    Create token.js

---
+++ utils/token.js
@@ -0,0 +1,6 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+exports.generateToken = (user) => {
+  return jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '2h' })
+}
